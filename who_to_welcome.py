fefacaca036015a5a642b0cd7a1c9c03c73c5e41
Jacob Gray <jacob@debonairstudios.com>
2015-06-11 08:58:53 -0400

    checks if a user is new or not
    call it like if check(message.user.name) == true:
    #user is new

---
+++ who_to_welcome.py
@@ -0,0 +1,25 @@
+def check_user(user):
+  new = true
+  try:
+    f = open("users.txt", "r")
+    try:
+        file = f.readlines()
+    finally:
+        f.close()
+  except IOError:
+    pass
+    
+  for x in len(file):
+    if user == file[x]:
+    	new=false
+    
+  if new==true:
+	  try:
+	      logfile = open("users.txt", "a")
+	      try:
+	          logfile.write(user)
+	      finally:
+	          logfile.close()
+	  except IOError:
+	      pass
+return new
