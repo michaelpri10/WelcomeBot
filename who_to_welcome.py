ae46c710df8441c2cc3ff633c50c235499e41a13
Jacob Gray <jacob@debonairstudios.com>
2015-06-11 09:02:11 -0400

    added indentation

--- who_to_welcome.py
+++ who_to_welcome.py
@@ -1,25 +1,25 @@
 def check_user(user):
   new = true
   try:
-    f = open("users.txt", "r")
+  	f = open("users.txt", "r")
     try:
-        file = f.readlines()
+      file = f.readlines()
     finally:
-        f.close()
+      f.close()
   except IOError:
     pass
     
   for x in len(file):
-    if user == file[x]:
-    	new=false
+  	if user == file[x]:
+      new=false
     
   if new==true:
+    try:
+	  logfile = open("users.txt", "a")
 	  try:
-	      logfile = open("users.txt", "a")
-	      try:
-	          logfile.write(user)
-	      finally:
-	          logfile.close()
+	    logfile.write(user)
+	  finally:
+	    logfile.close()
 	  except IOError:
-	      pass
+	    pass
 return new
