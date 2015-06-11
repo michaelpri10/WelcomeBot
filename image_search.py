8daf719708a2f3b483b6e8cc5c7dbb9e2140b3cf
Jacob Gray <jacob@debonairstudios.com>
2015-06-11 16:05:34 -0400

    fixed partially
    obviously not done, but it's a start

--- image_search.py
+++ image_search.py
@@ -2,7 +2,7 @@ import urllib2
 from html.parser import HTMLParser
 from html.entities import name2codepoint
 
-class parser(HTMLParser):
+class parserHTML(HTMLParser):
     def handle_starttag(self, tag, attrs):
         print("Start tag:", tag)
         for attr in attrs:
