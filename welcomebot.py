b72b3f805d4a430e106664a8cc431943a181a1d6
ProgramFOX <programfox@hotmail.be>
2015-06-13 19:57:01 +0200

    Threading for !!image

--- welcomebot.py
+++ welcomebot.py
@@ -6,6 +6,7 @@ import os
 import random
 import sys
 import time
+from threading import Thread
 
 import ChatExchange.chatexchange.client
 import ChatExchange.chatexchange.events
@@ -106,16 +107,19 @@ def on_command(message, client):
             if len(message.content.split()) == 1:
                 pass
             else:
-                search_term = "".join(message.content.split()[1:])
-                image = image_search.search_image(search_term)
-                print image
-                if image is False:
-                    print "No Image"
-                    room.send_message("@"+message.user.name.replace(" ","")+" No image was found for "+search_term)
-                else:
-                    print message.content
-                    print search_term
-                    room.send_message(image)
+                def perform_search():
+                    search_term = "".join(message.content.split()[1:])
+                    image = image_search.search_image(search_term)
+                    print image
+                    if image is False:
+                        print "No Image"
+                        room.send_message("@"+message.user.name.replace(" ","")+" No image was found for "+search_term)
+                    else:
+                        print message.content
+                        print search_term
+                        room.send_message(image)
+                t = Thread(target=perform_search)
+                t.start()
 
 def setup_logging():
     logging.basicConfig(level=logging.INFO)
