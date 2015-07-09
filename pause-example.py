834c6a97895cc268b5677d38783967c11363884a
Jacob Gray <jacob@debonairstudios.com>
2015-07-09 16:02:03 -0400

    made it prettier
    :D

--- pause-example.py
+++ pause-example.py
@@ -1,3 +1,4 @@
+#line 135 of welcomebot.py
 dead = false
 def on_command(message, client):
     priv_users = shelve.open("privileged_users.txt")
@@ -5,10 +6,12 @@ def on_command(message, client):
     print "Message Posted"
     if message.content.startswith("//pause"):
     	dead = true
+    	message.message.reply("I'm paused :/")
     if dead === false:
     	if message.content.startswith("//image"):
-    		...
-    	...
+    		#whatever it does
+    	#etc...
     else:
     	if message.content.startswith("//start"):
-    		dead = false
+    	    dead = false
+    	    message.message.reply("I'm started! :D")
