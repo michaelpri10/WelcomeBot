799ff39730b69ad0f4b6444200c368c35dfd092a
michaelpri <michaelpri10@gmail.com>
2015-07-09 17:26:20 -0400

    Fix pausing

--- welcomebot.py
+++ welcomebot.py
@@ -21,9 +21,9 @@ import who_to_welcome
 import image_search
 import weather_search
 import youtube_search
+from BotProperties import BotProperties
 
 logger = logging.getLogger(__name__)
-
 
 def main():
     setup_logging()
@@ -105,8 +105,7 @@ def main():
     print "(You are now in room #%s on %s.)" % (room_id, host_id)
     if "first_start" in sys.argv:
         room.send_message("I'm alive :) (running on commit: " + os.popen('git log --pretty=format:"%h" -n 1').read() + ")")
-    global paused
-    paused = False
+
     while True:
         message = raw_input("<< ")
         if message == "die":
@@ -120,158 +119,160 @@ def main():
 
 
 def on_event(event, client):
-    if isinstance(event, ChatExchange.chatexchange.events.UserEntered):
-        on_enter(event)
-    elif isinstance(event, ChatExchange.chatexchange.events.MessagePosted):
-        on_command(event, client)
+    if not BotProperties.paused:
+        if isinstance(event, ChatExchange.chatexchange.events.UserEntered):
+            on_enter(event)
+        elif isinstance(event, ChatExchange.chatexchange.events.MessagePosted):
+            on_command(event)
+    else:
+        priv_users = shelve.open("privileged_users.txt")
+        if isinstance(event, ChatExchange.chatexchange.events.MessagePosted) and event.content.startswith("//start"):
+            if (event.user.id == 121401 and host_id == 'stackexchange.com') or (event.user.id == 284141 and host_id == 'meta.stackexchange.com') or (event.user.id == 4087357 and host_id == 'stackoverflow.com') or (str(event.user.id) in priv_users[host_id+room_id]):
+                BotProperties.paused = False
+                event.message.reply("I am now resuming :)")
+        priv_users.close()
 
 def on_enter(event):
     print "User Entered"
-    if not paused:
-        if len(welcome_message) < 1:
+    if len(welcome_message) < 1:
+        pass
+    else:
+        if event.user.id == bot.id or event.user.reputation < 20:
             pass
         else:
-            if event.user.id == bot.id or event.user.reputation < 20:
-                pass
-            else:
-                if who_to_welcome.check_user(event.user.id, room_id, 'enter'):
-                    room.send_message("@" + event.user.name.replace(" ", "")+" "+ welcome_message)
+            if who_to_welcome.check_user(event.user.id, room_id, 'enter'):
+                room.send_message("@" + event.user.name.replace(" ", "")+" "+ welcome_message)
 
-def on_command(message, client):
+def on_command(message):
     priv_users = shelve.open("privileged_users.txt")
     print "Message Posted"
-    if not paused:
-        if message.content.startswith("//image"):
-            print "Is image request"
-            if len(message.content.split()) == 1:
-                message.message.reply("No search term given")
-            else:
-                def perform_image_search():
-                    search_term = "+".join(message.content.split()[1:])
-                    image = image_search.search_image(search_term)
-                    print image
-                    if image is False:
-                        print "No Image"
-                        message.message.reply("No image was found for " + search_term)
-                    else:
-                        print message.content
-                        print search_term
-                        message.message.reply(image)
-                t = Thread(target=perform_image_search)
-                t.start()
-        elif message.content.startswith("//alive"):
-            print "Is alive request"
-            message.message.reply("Indeed :D")
-            
-        elif message.content.startswith("//die"):
-            if (message.user.id == 121401 and host_id == 'stackexchange.com') or (message.user.id == 284141 and host_id == 'meta.stackexchange.com') or (message.user.id == 4087357 and host_id == 'stackoverflow.com') or (str(message.user.id) in priv_users[host_id+room_id]):
-                message.message.reply("I'm dead :(")
-                time.sleep(0.4)
-                os._exit(6)
-            else:
-                message.message.reply("You are not authorized kill me!!! Muahaha!!!! Please contact `@michaelpri` if I am acting up")
-        elif message.content.startswith("//reset"):
-            if (message.user.id == 121401 and host_id == 'stackexchange.com') or (message.user.id == 284141 and host_id == 'meta.stackexchange.com') or (message.user.id == 4087357 and host_id == 'stackoverflow.com') or (str(message.user.id) in priv_users[host_id+room_id]):
-                message.message.reply("Resetting...")
-                time.sleep(0.4)
-                os._exit(5)
-            else:
-                message.message.reply("You are not authorized reset me. Please contatct `@michaelpri` if I need resetting")
-        elif message.content.startswith("//pull"):
-            if (message.user.id == 121401 and host_id == 'stackexchange.com') or (message.user.id == 284141 and host_id == 'meta.stackexchange.com') or (message.user.id == 4087357 and host_id == 'stackoverflow.com') or (str(message.user.id) in priv_users[host_id+room_id]):
-                os._exit(3)
-            else:
-                message.message.reply("You are not authorized to pull. Please contatct `@michaelpri` if I need some pulling")
-
-        elif message.content.startswith("//priv"):
-            if (message.user.id == 121401 and host_id == 'stackexchange.com') or (message.user.id == 284141 and host_id == 'meta.stackexchange.com') or (message.user.id == 4087357 and host_id == 'stackoverflow.com') or (str(message.user.id) in priv_users[host_id+room_id]):
-                if len(message.content.split()) == 2:
-                    user_to_priv = message.content.split()[1]
-                    if (host_id+room_id) not in priv_users:
-                        priv_users[host_id+room_id] = []
-                    if user_to_priv in priv_users[host_id+room_id]:
-                        message.message.reply("User already privileged")
-                    else:
-                        priv_users[host_id+room_id] += [user_to_priv]
-                        message.message.reply("User " + user_to_priv + " added to privileged users for room " + room_id + " on chat." + host_id)
+    if message.content.startswith("//image"):
+        print "Is image request"
+        if len(message.content.split()) == 1:
+            message.message.reply("No search term given")
+        else:
+            def perform_image_search():
+                search_term = "+".join(message.content.split()[1:])
+                image = image_search.search_image(search_term)
+                print image
+                if image is False:
+                    print "No Image"
+                    message.message.reply("No image was found for " + search_term)
                 else:
-                    message.message.reply("Invalid privilege giving")
-            else:
-                message.message.reply("You are not authorized to add privileged users :( Please contact `@michaelpri` if someone needs priviliges")
+                    print message.content
+                    print search_term
+                    message.message.reply(image)
+            t = Thread(target=perform_image_search)
+            t.start()
+    elif message.content.startswith("//alive"):
+        print "Is alive request"
+        message.message.reply("Indeed :D")
 
-        elif message.content.startswith("//choose"):
-            print "Is choose request"
-            if len(message.content.split()) == 1:
-                message.message.reply("No choices given")
-            else:
-                def perform_choose_request():
-                    if " or " in message.content:
-                        choices = message.content[8:].split(" or ")
-                        message.message.reply("I choose "+random.choice(choices))
-                    else:
-                        message.message.reply("I'm not sure what your options are")
-                c = Thread(target=perform_choose_request)
-                c.start()
+    elif message.content.startswith("//die"):
+        if (message.user.id == 121401 and host_id == 'stackexchange.com') or (message.user.id == 284141 and host_id == 'meta.stackexchange.com') or (message.user.id == 4087357 and host_id == 'stackoverflow.com') or (str(message.user.id) in priv_users[host_id+room_id]):
+            message.message.reply("I'm dead :(")
+            time.sleep(0.4)
+            os._exit(6)
+        else:
+            message.message.reply("You are not authorized kill me!!! Muahaha!!!! Please contact `@michaelpri` if I am acting up")
+    elif message.content.startswith("//reset"):
+        if (message.user.id == 121401 and host_id == 'stackexchange.com') or (message.user.id == 284141 and host_id == 'meta.stackexchange.com') or (message.user.id == 4087357 and host_id == 'stackoverflow.com') or (str(message.user.id) in priv_users[host_id+room_id]):
+            message.message.reply("Resetting...")
+            time.sleep(0.4)
+            os._exit(5)
+        else:
+            message.message.reply("You are not authorized reset me. Please contatct `@michaelpri` if I need resetting")
+    elif message.content.startswith("//pull"):
+        if (message.user.id == 121401 and host_id == 'stackexchange.com') or (message.user.id == 284141 and host_id == 'meta.stackexchange.com') or (message.user.id == 4087357 and host_id == 'stackoverflow.com') or (str(message.user.id) in priv_users[host_id+room_id]):
+            os._exit(3)
+        else:
+            message.message.reply("You are not authorized to pull. Please contatct `@michaelpri` if I need some pulling")
 
-        elif message.content.startswith("//help"):
-            print "Is help request"
-            message.message.reply("""My Commands
-                                 - //image (image search term)
-                                 - //choose (choice) or (choice) [or choice...]
-                                 - //weather (city)[, country/state]
-                                 - //youtube (youtube search term)
-                              """)
-
-        elif message.content.startswith("//weather"):
-            print "Is weather request"
-            if len(message.content.split()) == 1:
-                message.message.reply("City and country/state not given")
+    elif message.content.startswith("//priv"):
+        if (message.user.id == 121401 and host_id == 'stackexchange.com') or (message.user.id == 284141 and host_id == 'meta.stackexchange.com') or (message.user.id == 4087357 and host_id == 'stackoverflow.com') or (str(message.user.id) in priv_users[host_id+room_id]):
+            if len(message.content.split()) == 2:
+                user_to_priv = message.content.split()[1]
+                if (host_id+room_id) not in priv_users:
+                    priv_users[host_id+room_id] = []
+                if user_to_priv in priv_users[host_id+room_id]:
+                    message.message.reply("User already privileged")
+                else:
+                    priv_users[host_id+room_id] += [user_to_priv]
+                    message.message.reply("User " + user_to_priv + " added to privileged users for room " + room_id + " on chat." + host_id)
             else:
-                def perform_weather_search():
-                    city_and_country = [i.replace(" ", "%20") for i in message.content[10:].split(",")]
-                    city = city_and_country[0]
-                    if len(city_and_country) == 2:
-                        country_state = city_and_country[1]
-                        print city, country_state
-                        try:
-                            message.message.reply(weather_search.weather_search(city, country_state))
-                        except:
-                            message.message.reply("Couldn't find weather info for " + message.content)
-                    elif len(city_and_country) == 1:
-                        try:
-                            message.message.reply(weather_search.weather_search(city, ""))
-                        except:
-                            message.message.reply("Couldn't find weather info for " + message.content)
-                w = Thread(target=perform_weather_search)
-                w.start()
+                message.message.reply("Invalid privilege giving")
+        else:
+            message.message.reply("You are not authorized to add privileged users :( Please contact `@michaelpri` if someone needs priviliges")
 
-        elif message.content.startswith("//youtube"):
-            print "Is youtube request"
-            if len(message.content.split()) == 1:
-                message.message.reply("No search term given")
-            else:
-                def perform_youtube_search():
-                    search_term = "+".join(message.content.split()[1:])
-                    video = youtube_search.youtube_search(search_term)
-                    print video
-                    if video is False:
-                        print "No Image"
-                        message.message.reply("No video was found for " + search_term)
-                    else:
-                        print message.content
-                        print search_term
-                        message.message.reply(video)
-                v = Thread(target=perform_youtube_search)
-                v.start()
-        elif message.content.startswith("//pause"):
-            if (message.user.id == 121401 and host_id == 'stackexchange.com') or (message.user.id == 284141 and host_id == 'meta.stackexchange.com') or (message.user.id == 4087357 and host_id == 'stackoverflow.com') or (str(message.user.id) in priv_users[host_id+room_id]):
-                paused = True
-                message.message.reply("I am now paused :|")
-    else:
-        if message.content.startswith("//start"):
-            if (message.user.id == 121401 and host_id == 'stackexchange.com') or (message.user.id == 284141 and host_id == 'meta.stackexchange.com') or (message.user.id == 4087357 and host_id == 'stackoverflow.com') or (str(message.user.id) in priv_users[host_id+room_id]):
-                paused = False
-                message.message.reply("I am now resuming :)")
+    elif message.content.startswith("//choose"):
+        print "Is choose request"
+        if len(message.content.split()) == 1:
+            message.message.reply("No choices given")
+        else:
+            def perform_choose_request():
+                if " or " in message.content:
+                    choices = message.content[8:].split(" or ")
+                    message.message.reply("I choose "+random.choice(choices))
+                else:
+                    message.message.reply("I'm not sure what your options are")
+            c = Thread(target=perform_choose_request)
+            c.start()
+
+    elif message.content.startswith("//help"):
+        print "Is help request"
+        message.message.reply("""My Commands
+                             - //image (image search term)
+                             - //choose (choice) or (choice) [or choice...]
+                             - //weather (city)[, country/state]
+                             - //youtube (youtube search term)
+                          """)
+
+    elif message.content.startswith("//weather"):
+        print "Is weather request"
+        if len(message.content.split()) == 1:
+            message.message.reply("City and country/state not given")
+        else:
+            def perform_weather_search():
+                city_and_country = [i.replace(" ", "%20") for i in message.content[10:].split(",")]
+                city = city_and_country[0]
+                if len(city_and_country) == 2:
+                    country_state = city_and_country[1]
+                    print city, country_state
+                    try:
+                        message.message.reply(weather_search.weather_search(city, country_state))
+                    except:
+                        message.message.reply("Couldn't find weather info for " + message.content)
+                elif len(city_and_country) == 1:
+                    try:
+                        message.message.reply(weather_search.weather_search(city, ""))
+                    except:
+                        message.message.reply("Couldn't find weather info for " + message.content)
+            w = Thread(target=perform_weather_search)
+            w.start()
+
+    elif message.content.startswith("//youtube"):
+        print "Is youtube request"
+        if len(message.content.split()) == 1:
+            message.message.reply("No search term given")
+        else:
+            def perform_youtube_search():
+                search_term = "+".join(message.content.split()[1:])
+                video = youtube_search.youtube_search(search_term)
+                print video
+                if video is False:
+                    print "No Image"
+                    message.message.reply("No video was found for " + search_term)
+                else:
+                    print message.content
+                    print search_term
+                    message.message.reply(video)
+            v = Thread(target=perform_youtube_search)
+            v.start()
+    elif message.content.startswith("//pause"):
+        if (message.user.id == 121401 and host_id == 'stackexchange.com') or (message.user.id == 284141 and host_id == 'meta.stackexchange.com') or (message.user.id == 4087357 and host_id == 'stackoverflow.com') or (str(message.user.id) in priv_users[host_id+room_id]):
+            BotProperties.paused = True
+            message.message.reply("I am now paused :|")
+
     priv_users.close()
 
 
