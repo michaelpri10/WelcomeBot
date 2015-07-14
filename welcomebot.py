2cc787682c2121ec86f7dd80536d024a43f3d447
Jacob Gray <jacob@debonairstudios.com>
2015-07-14 19:37:05 -0400

    updated to new help
    this had better work, if I have messed up my indenting I apologize

--- welcomebot.py
+++ welcomebot.py
@@ -223,15 +223,29 @@ def on_command(message):
                 message.message.reply("I choose " + random.choice(choices))
             else:
                 message.message.reply("I'm not sure what your options are")
-    elif message.content.startswith("//help"):
+     elif message.content.startswith("//help"):
         print "Is help request"
-        message.message.reply("""My Commands
-                             - //image (image search term)
-                             - //choose (choice) or (choice) [or choice...]
-                             - //weather (city)[, country/state]
-                             - //youtube (youtube search term)
-                             - //source
-                          """)
+        if (message.user.id == 121401 and host_id == 'stackexchange.com') or (message.user.id == 284141 and host_id == 'meta.stackexchange.com') or (message.user.id == 4087357 and host_id == 'stackoverflow.com') or (str(message.user.id) in priv_users[host_id + room_id]):
+            message.message.reply("""My Commands
+                                     - //image (image search term)
+                                     - //choose (choice) or (choice) [or choice...]
+                                     - //weather (city)[, country/state]
+                                     - //youtube (youtube search term)
+                                     - //source
+                                     - //pull *
+                                     - //pause *
+                                     - //start *
+                                     - //die *
+                                     * = Only Privileged Users
+                                """)
+        else:
+            message.message.reply("""My Commands
+                                 - //image (image search term)
+                                 - //choose (choice) or (choice) [or choice...]
+                                 - //weather (city)[, country/state]
+                                 - //youtube (youtube search term)
+                                 - //source
+                              """)
     elif message.content.startswith("//weather"):
         print "Is weather request"
         if len(message.content.split()) == 1:
