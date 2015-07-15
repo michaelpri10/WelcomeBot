9e44f7acdaf05d2b6650dc7b065ce37afda9c7c3
Jacob Gray <jacob@debonairstudios.com>
2015-07-15 11:42:48 -0400

    added random number

--- welcomebot.py
+++ welcomebot.py
@@ -177,7 +177,7 @@ def on_command(message):
     
     elif message.content.startswith("//testenter"):
         print "Is testenter request"
-        room.send_message("@skynetTestUser " + BotProperties.welcome_message)
+        room.send_message("@skynetTestUser" + randint(0,9) +" " + BotProperties.welcome_message)
 
     elif message.content.startswith("//info"):
         print "Is info request"
