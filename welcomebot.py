f2b4542c558d94ba56c37f8ce8206716627acdb0
Jacob Gray <jacob@debonairstudios.com>
2015-07-14 22:17:55 -0400

    added help so it posts two messages

--- welcomebot.py
+++ welcomebot.py
@@ -243,6 +243,13 @@ def on_command(message):
                                  - //youtube (youtube search term)
                                  - //source
                               """)
+        if (message.user.id == 121401 and host_id == 'stackexchange.com') or (message.user.id == 284141 and host_id == 'meta.stackexchange.com') or (message.user.id == 4087357 and host_id == 'stackoverflow.com') or (str(message.user.id) in priv_users[host_id + room_id]):
+            message.message.reply("""You are a Priveleged User, so you can use these commands:
+                                     - //pull
+                                     - //pause
+                                     - //start
+                                     - //die
+                                """)
     elif message.content.startswith("//weather"):
         print "Is weather request"
         if len(message.content.split()) == 1:
