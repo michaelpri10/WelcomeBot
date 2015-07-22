b72e974e07480c97aefafe67700bf588f1aa7694
Jacob Gray <jacob@debonairstudios.com>
2015-07-22 07:49:14 -0400

    Update ddg.py

--- ddg.py
+++ ddg.py
@@ -3,6 +3,9 @@ import json
 
 def search(string):
   ddg = urllib2.urlopen("https://api.duckduckgo.com/?q=" + string.replace(" ","+") + "&format=json&pretty=1")
-  result = json.load(ddg)
+  result = {}
+  result["results"] = json.load(ddg)
+  result["term"] = "https://api.duckduckgo.com/?q=" + string.replace(" ","+") + "&format=json&pretty=1"
+  
   return result
   
