f53b90bda8b11a50ba809a15756ece04251386af
Jacob Gray <jacob@debonairstudios.com>
2015-07-24 21:10:22 -0400

    Update ddg.py

--- ddg.py
+++ ddg.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 def search(string):
   send = {}
-  site = "http://www.google.com/search?tbm=isch&safe=strict&q=" + search_term
+  site = "http://www.google.com/search?tbm=isch&safe=strict&q=" + string
   print site
   hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3', 'Accept-Encoding': 'none', 'Accept-Language': 'en-US,en;q=0.8', 'Connection': 'keep-alive'}
   req = urllib2.Request(site, headers=hdr)
