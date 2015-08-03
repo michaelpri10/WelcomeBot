83adfabbc1acd4a3fe5672f7be98d39ae849c54e
ProgramFOX <programfox@hotmail.be>
2015-08-03 10:30:27 +0200

    Avoid UnicodeEncodeError at print

--- image_search.py
+++ image_search.py
@@ -7,7 +7,7 @@ import re
 def search_image(search_term):
 
     site = "http://www.google.com/search?tbm=isch&safe=strict&q=" + search_term
-    print site
+    print site.encode('ascii', errors='replace')
     hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3', 'Accept-Encoding': 'none', 'Accept-Language': 'en-US,en;q=0.8', 'Connection': 'keep-alive'}
 
     req = urllib2.Request(site, headers=hdr)
